db41057a252373d2e9781a728c22c0f43e5e1cca
Lauren Dahlenburg <laurenyockey@gmail.com>
2019-06-26 22:59:54 -0500

    add waypoint to bio

--- js/main.js
+++ js/main.js
@@ -6,5 +6,19 @@ $(document).ready(function () {
             imageSrc: 'img/typing.jpg'
     });
 
+    // WAYPOINTS
+        $('.bio ul li')
+              .css('opacity', 0) // immediately hide element
+              .waypoint(function(direction) {
+                if (direction === 'down') {
+                  $(this.element).animate({ opacity: 1 })
+                }
+                else {
+                  $(this.element).animate({ opacity: 0.3 })
+                }
+              }, {
+                offset: '65%'
+              })
+
     // Don't code below this line
 }); //END DOC.READY
