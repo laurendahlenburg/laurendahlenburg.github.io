e7c4be00445f61474a925291559318dd33cb3f27
Lauren Dahlenburg <laurenyockey@gmail.com>
2019-06-26 22:31:41 -0500

    add basic styles and waypoints js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
     //Don't code above this line
 
+    $('header').parallax(
+        {
+            imageSrc: 'img/typing.jpg'
+    });
+
     // Don't code below this line
 }); //END DOC.READY
