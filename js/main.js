1cfc9e33f5a1cea4c07a8a3cfe33468fdf8df43e
Lauren Dahlenburg <laurenyockey@gmail.com>
2019-07-04 15:32:39 -0500

    add skills section

--- js/main.js
+++ js/main.js
@@ -3,22 +3,37 @@ $(document).ready(function () {
 
     $('header').parallax(
         {
-            imageSrc: 'img/typing.jpg'
+            imageSrc: 'img/laptop.jpg'
+    });
+    $('.parallax-2').parallax(
+        {
+            imageSrc: 'img/buie.jpg'
     });
 
     // WAYPOINTS
-        $('.bio ul li')
-              .css('opacity', 0) // immediately hide element
-              .waypoint(function(direction) {
-                if (direction === 'down') {
-                  $(this.element).animate({ opacity: 1 })
-                }
-                else {
-                  $(this.element).animate({ opacity: 0.3 })
-                }
-              }, {
-                offset: '65%'
-              })
+
+    var $hello = $('.bio h2');
+    $hello.addClass('hide');
+
+    $hello.waypoint(function() {
+        $hello.removeClass('hide').addClass('animated slideInUp')
+        }, {
+            offset: '65%'
+    });
+
+    $('.bio ul li')
+        .css('opacity', 0) // immediately hide element
+        .waypoint(function(direction) {
+            if (direction === 'down') {
+              $(this.element).animate({ opacity: 1 })
+            }
+            else {
+                $(this.element).animate({ opacity: 0.3 })
+            }
+            }, {
+            offset: '65%'
+        });
+
 
     // Don't code below this line
 }); //END DOC.READY
