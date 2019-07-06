c4c851e76fec9937f99079794a289bc234a4cc43
Lauren Dahlenburg <laurenyockey@gmail.com>
2019-07-05 21:01:50 -0500

    remove existing waypoints

--- js/main.js
+++ js/main.js
@@ -13,27 +13,27 @@ $(document).ready(function () {
 
     // WAYPOINTS
 
-    var $hello = $('.bio h2');
-    $hello.addClass('hide');
-
-    $hello.waypoint(function() {
-        $hello.removeClass('hide').addClass('animated slideInUp')
-        }, {
-            offset: '65%'
-    });
-
-    $('.bio ul li')
-        .css('opacity', 0) // immediately hide element
-        .waypoint(function(direction) {
-            if (direction === 'down') {
-              $(this.element).animate({ opacity: 1 })
-            }
-            else {
-                $(this.element).animate({ opacity: 0.3 })
-            }
-            }, {
-            offset: '65%'
-        });
+    // var $hello = $('.bio h2');
+    // $hello.addClass('hide');
+    //
+    // $hello.waypoint(function() {
+    //     $hello.removeClass('hide').addClass('animated slideInUp')
+    //     }, {
+    //         offset: '65%'
+    // });
+    //
+    // $('.bio ul li')
+    //     .css('opacity', 0) // immediately hide element
+    //     .waypoint(function(direction) {
+    //         if (direction === 'down') {
+    //           $(this.element).animate({ opacity: 1 })
+    //         }
+    //         else {
+    //             $(this.element).animate({ opacity: 0.3 })
+    //         }
+    //         }, {
+    //         offset: '65%'
+    //     });
 
 
     // Don't code below this line
