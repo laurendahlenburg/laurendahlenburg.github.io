dae3fd6350ee10e34e4187a0172be81419db205d
Lauren Dahlenburg <laurenyockey@gmail.com>
2019-08-20 22:00:39 -0500

    darken color scheme

--- js/main.js
+++ js/main.js
@@ -9,6 +9,11 @@ $(function() {
         }
     );
 
+    $(document).scroll(function () {
+        var nav = $(".fixed-top");
+
+        nav.toggleClass('scrolled', $(this).scrollTop() > nav.height());
+    });
 
     // Stop coding
 });
